3f38d0a6983d178604975797b8837471104780df
Sachink03 <prajapatsachin835@gmail.com>
2025-05-01 16:54:37 +0530

    sach

--- src/routes/web.js
+++ src/routes/web.js
@@ -23,6 +23,7 @@ router.get('/fetchserver', authMiddleware, UserController.fetchserver);
 router.post('/submitserver', authMiddleware, UserController.submitserver);
 router.get('/fetchrenew', authMiddleware, UserController.fetchrenew);
 router.post('/renew-server', authMiddleware, UserController.renewserver);
+router.get('/fetchservers', authMiddleware, UserController.fetchservers);
 // router.post('/register', (req, res) => {
 //   res.json({ message: 'Welcome to regiset' });
 // });
